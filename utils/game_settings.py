f5f26985e1c11df0b9677727d337bf49c5ae6649
Rebecca W Silverstein <rswhipple@gmail.com>
2024-06-02 15:17:28 -0700

    update: improve user prompts for clarity

--- utils/game_settings.py
+++ utils/game_settings.py
@@ -19,7 +19,7 @@ class GameSettings:
             self.series_mode = True
     
     def _set_players(self):
-        question = "How many players?\n 1 or 2?: "
+        question = "How many players (1 or 2)? "
         
         while True:
             try:
@@ -59,9 +59,9 @@ class GameSettings:
     def _print_settings(self):
         print(
             "\nGame Settings:"
-            f"\ngame_mode = {self.series_mode}"
-            f"\nmulti_mode = {self.multi_mode}"
-            f"\nscore_mode = {self.score_mode}"
+            # f"\ngame_mode = {self.series_mode}"
+            # f"\nmulti_mode = {self.multi_mode}"
+            # f"\nscore_mode = {self.score_mode}"
             f"\nnum_players = {self.num_players}"
             f"\nlevel = {self.level}\n"
             )
@@ -69,7 +69,7 @@ class GameSettings:
     def _run(self):
         # self._set_mode()
         self._set_level() 
-        self._set_score()
+        # self._set_score()
         self._set_players()
 
         # TEST: print settings
